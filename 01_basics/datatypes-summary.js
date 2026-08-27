d66e6ba37efaa3c217044f18cd328f91f6afaac0
krinshuk05 <krinshuksingh105@gmail.com>
2026-08-27 17:02:40 +0000

    summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,16 @@
+const id=Symbol("232")
+const anotherId=Symbol("232")
+console.log(id==anotherId);
+
+const bigNum=32874953253323552355323454
+console.log(bigNum)
+
+const heros=["shaktiman", "nagraj", "doga"];
+let myObj={
+    name: "krinshuk",
+    age:19
+}
+const myFunction =function(){
+    console.log("hello world");
+}
+console.log(typeof heros)
