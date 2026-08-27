c9968c44e18c883831a3c28d600ac9beb9a083d4
krinshuk05 <krinshuksingh105@gmail.com>
2026-08-27 08:43:43 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail= "krinshuk@gmail.com"
+var accountPassword="12345"
+accountCity="Noida" //variable can be declared like this too.
+let accountState // no value is assigned so this will be treated as undefined.
+// accountId =2
+accountEmail="hacauhc@.com"
+accountPassword="13213211"
+accountCity="Delhi"
+console.table([accountId,accountEmail,accountPassword, accountCity, accountState])
+/*
+Prefer not to use var
+ coz of issue with block scope and functional scope.
+*/
