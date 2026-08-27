378b07c9e26f65ca1420d236ae140332365c0cfb
krinshuk05 <krinshuksingh105@gmail.com>
2026-08-27 17:24:06 +0000

    heap and stack

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -14,3 +14,27 @@ const myFunction =function(){
     console.log("hello world");
 }
 console.log(typeof heros)
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// stack(primitve) yha se variable ki copy mitli haii, 
+// heap(non-primitive) yha variable ka ref milta haii yha original value mein changes hogi
+
+let myYoutubename="rudra777"
+let anotherName=myYoutubename
+anotherName="Pro_gamerz"
+console.log(anotherName)
+console.log(myYoutubename)
+
+
+let user1={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+let user2=user1
+user2.email= "freefire@google.com"
+
+console.log(user1.email)
+console.log(user2.email)
