158c5b3c7062396573cfd26fc7bc090d9f104525
krinshuk05 <krinshuksingh105@gmail.com>
2026-08-27 08:44:13 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict" //treat all js codes as a newer version
+//alert(3+3) //we are using nodejs, not browser
+console.log(3
+    +3) //not a good practie coz code readability shd be high 
+
+
+    let name ="Krinshuk" //string
+    let age= 19 //int
+    let isLoggedIn= false  //boolean
+
+
+    //primitve data types
+    // number => 2 to power 53
+    // bigint=> can be used in the trading website to get the higher numbers than int
+    //  data type
+    // string can be accessed in both "" & ''.
+    //boolean=> true or false
+    //null => standalone value
+    //undefined => value not assigned currently
+    //symbol=> unique
+
+    
+    //object
+    
+    console.log(typeof "Krinshuk");
+    console.log(typeof age);
+    console.log(typeof(null)); // null object haii nd ye interview question bn skta haii
+    console.log(typeof(undefined)); // ye khud mein ek type haii
+
+    
