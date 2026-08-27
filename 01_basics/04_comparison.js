1d4b37e5ca207d56e43c9e04f32c92cd8ac5d9d4
krinshuk05 <krinshuksingh105@gmail.com>
2026-08-27 15:41:52 +0000

    comaprisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,30 @@
+console.log("2">1)  // yha dono mein 2 number automamtically num 2 k andr convert ho rh
+console.log("02">1)
+
+//typescript rule deta haii ki tm do alg data types ko compare mt kroo
+
+
+console.log(null>0); //1
+console.log(null==0); //2
+console.log(null>=0); //3
+
+// equality check == and comparsions >,<,<=,=>,work differently
+// comparison convert null to a number, treating it as 0
+// that's why (3) null>=0 is true and null>0 is false
+
+
+console.log(undefined ==0 );
+console.log(undefined >0 );
+console.log(undefined >=0 );
+
+
+// === strtict check krta haiii
+// == bss value check krta haii while === value or data type dono check krta haii
+
+console.log("2"==2);
+console.log("2"===2);
+
+
+
+
+
